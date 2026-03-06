e5d5be141ead61f04f32f0d966da0be87489ccbb
nisargamd005-ai <nisargamd005@gmail.com>
2026-03-06 20:41:18 +0530

    fix gunicorn start command

--- ai_teacher/ai_teacher/wsgi.py
+++ ai_teacher/ai_teacher/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_teacher.settings')
